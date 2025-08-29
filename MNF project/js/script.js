b27d6bb0747fb940011459b10807053f708b21bd
Kelwin Gil Fernandes <kelwingilfernandes18@gmail.com>
2025-08-29 10:19:53 -0300

    Update script.js

---
+++ MNF project/js/script.js
@@ -0,0 +1,6 @@
+// js/script.js
+document.getElementById("login-link").addEventListener("click", function(e) {
+    e.preventDefault();
+    alert("Área de login em desenvolvimento!");
+    // Aqui você pode adicionar um popup ou redirecionar depois
+});

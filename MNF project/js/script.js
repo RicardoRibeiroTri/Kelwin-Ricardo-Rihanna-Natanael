f85ff2c83ac01c75ca9ec8c955a81f2d8c8b9a73
RicRibeiroTri <ricardoribeirotrindade273@gmail.com>
2025-10-24 08:46:57 -0300

    Delete MNF project directory

--- MNF project/js/script.js
+++
@@ -1,6 +0,0 @@
-// js/script.js
-document.getElementById("login-link").addEventListener("click", function(e) {
-    e.preventDefault();
-    alert("Área de login em desenvolvimento!");
-    // Aqui você pode adicionar um popup ou redirecionar depois
-});
